b65bc8c23c20685a24bd7485424b99091e9d0815
Jansen Gunderson <jansen@Jansens-MacBook-Pro.local>
2018-05-17 10:01:29 -0600

    submit button

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -15,6 +15,12 @@ class Playlist extends React.Component {
     this.props.onNameChange(name);
   }
 
+  buttonSuccess() {
+    this.setState(
+      <a className="Playlist-save" onclick={this.props.onSave} onSubmit={this.buttonSuccess}>Saved to Spotify!</a>
+    )
+  }
+
 
 
   render() {
@@ -22,7 +28,7 @@ class Playlist extends React.Component {
       <div className="Playlist">
         <input value={this.props.playlistName} onChange={this.handleNameChange}/>
           <TrackList tracks={this.props.playlistTracks} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={true}/>
-        <a className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</a>
+        <a className="Playlist-save" onClick={this.props.onSave} onSubmit={this.buttonSuccess}>SAVE TO SPOTIFY</a>
       </div>
     );
   }
